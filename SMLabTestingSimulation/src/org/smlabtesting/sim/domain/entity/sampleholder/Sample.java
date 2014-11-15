1f04d03f7159330963ae832e346bc68ab335dfa5
Ahmed El-Hajjar <me@cdahmedeh.net>
2014-11-15 16:59:16 -0500

    Added random sequence for Sample.

--- SMLabTestingSimulation/src/org/smlabtesting/sim/domain/entity/sampleholder/Sample.java
+++ SMLabTestingSimulation/src/org/smlabtesting/sim/domain/entity/sampleholder/Sample.java
@@ -1,10 +1,16 @@
 package org.smlabtesting.sim.domain.entity.sampleholder;
 
+import static org.smlabtesting.sim.executor.Simulation.DEFAULT_RNG;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
+import org.apache.commons.math3.distribution.EnumeratedIntegerDistribution;
+import org.apache.commons.math3.distribution.IntegerDistribution;
 import org.smlabtesting.sim.domain.generic.Entity;
+import org.smlabtesting.sim.executor.Simulation;
 
 /**
  * Maps to iC.Sample
@@ -12,9 +18,30 @@ import org.smlabtesting.sim.domain.generic.Entity;
  * @author Ahmed El-Hajjar
  */
 public class Sample extends Entity {
+    // Constants
+    private static final int[] SEQUENCE_ID = {0,1,2,3,4,5,6,7,8};
+    private static final double[] PROBABILITIES = {0.09, 0.13, 0.15, 0.12, 0.07, 0.11, 0.14, 0.06, 0.13}; 
+    
+    private static final Integer[][] SEQUENCES = {
+        { 1, 2, 4, 5 },
+        { 3, 4, 5 },
+        { 1, 2, 3, 4 },
+        { 4, 3, 2 },
+        { 2, 5, 1 },
+        { 4, 5, 2, 3 },
+        { 1, 5, 3, 4 },
+        { 5, 3, 1 },
+        { 2, 4, 5 }
+    };
+    
+    // RNGs
+    private static final IntegerDistribution distribution = new EnumeratedIntegerDistribution(DEFAULT_RNG, SEQUENCE_ID, PROBABILITIES);
+	
     // Factories
     public static Sample generateSample() {
-        return new Sample();
+        Sample sample = new Sample();
+        sample.testSequence.addAll(Arrays.asList(SEQUENCES[distribution.sample()])); // TODO: Needs to be reversed
+        return sample;
     }
 
     // Attributes
@@ -22,7 +49,6 @@ public class Sample extends Entity {
     
     // Constructs
     public Sample() {
-        IntStream.of(5,3,1,2,3,4,2,1).forEach(testSequence::add);
     }    
     
     // Entity API
