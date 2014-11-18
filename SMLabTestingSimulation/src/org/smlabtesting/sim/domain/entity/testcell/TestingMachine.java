8803d46084cccbc38e87d74c0b66cd8fc6f3beac
Ahmed El-Hajjar <me@cdahmedeh.net>
2014-11-18 13:18:38 -0500

    Added times to testing machine. (Some still not random.)

--- SMLabTestingSimulation/src/org/smlabtesting/sim/domain/entity/testcell/TestingMachine.java
+++ SMLabTestingSimulation/src/org/smlabtesting/sim/domain/entity/testcell/TestingMachine.java
@@ -2,6 +2,10 @@ package org.smlabtesting.sim.domain.entity.testcell;
 
 import static org.smlabtesting.sim.domain.entity.testcell.TestingMachine.TestingMachineState.*;
 
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+
+import org.apache.commons.math3.distribution.TriangularDistribution;
 import org.smlabtesting.sim.domain.entity.racetrack.RacetrackLine;
 import org.smlabtesting.sim.domain.entity.sampleholder.SampleHolder;
 import org.smlabtesting.sim.domain.generic.Entity;
@@ -21,7 +25,13 @@ public class TestingMachine extends Entity {
     }
     
     // Constants
+    private static final double[] TESTING_CYCLE_TIMES = DoubleStream.of(0, 0.77, 0.85 , 1.04, 1.24 , 1.7).map(i -> i * 60).toArray();
+    private static final int[] MACHINE_MBTF = IntStream.of(0, 14, Integer.MAX_VALUE, 9, 15, 16).map(i -> i * 3600).toArray();
+    private static final int[] MACHINE_MBTR = IntStream.of(0, 11, Integer.MAX_VALUE, 7, 14, 13).map(i -> i * 60).toArray();
     private static final int STATION_2_CLEANING_THRESHOLD = 300;
+    
+    // RNG
+    private static final TriangularDistribution stationTwoCleaningTimeDistribution = new TriangularDistribution(5.0*60, 6.0*60, 10.0*60);
     
     // Relationships
     private final TestCellBuffer testCellBuffer;
@@ -139,19 +149,19 @@ public class TestingMachine extends Entity {
     // Private helper methods
     
     private int generateTestingTime() {
-        return 100; //TODO: Hard-coded
+        return (int) TESTING_CYCLE_TIMES[machineId];
     }
 
     private int generateRepairTime() {
-        return 100; //TODO: Hard-coded
+        return MACHINE_MBTR[machineId]; //TODO: No randomization yet.
     }
 
     private int generateFailureTime() {
-        return 1000; //TODO: Hard-coded
+        return MACHINE_MBTF[machineId]; //TODO: No randomization yet.
     }
 
     private int generateCleaningTime() {
-        return 100; //TODO: Hard-coded
+        return (int) stationTwoCleaningTimeDistribution.sample();
     }
     
 }
