1624ca564740fa9c613b8a46ef7998cc13ada47f
Ahmed El-Hajjar <me@cdahmedeh.net>
2014-11-20 15:22:54 -0500

    Added comments for constants.

--- SMLabTestingSimulation_ABSModJ/src/org/smlabtesting/simabs/variable/Constants.java
+++ SMLabTestingSimulation_ABSModJ/src/org/smlabtesting/simabs/variable/Constants.java
@@ -3,15 +3,17 @@ package org.smlabtesting.simabs.variable;
 public class Constants {
 	// How much runs for station 2 in order for it to be cleaned.
 	public static final int STATION_2_CLEANING_THRESHOLD = 300;
-	// Constants
+	
+	// The capacity of all test cell buffer queues.
 	public static final int TEST_CELL_BUFFER_CAPACITY = 3;
-	// Constants
+	
+	// The capacity of the unload buffer.
 	public static final int UNLOADBUFFER_CAPACITY = 5;
-	// Constants
+	
+	// How many slots fit in the racetrack.
 	public static final int BELT_SLOTS_COUNT = 48;
+	
 	// Enter/Exit points for the load/unload machines and test cells.
-	public static final int[] STATION_EXITS = {0, 8, 16, 24, 32, 40};
-	public static final int[] STATION_ENTRANCES = {4, 12, 20, 28, 36, 44};
-	
-	
+	public static final int[] STATION_EXITS = {0, 8, 16, 24, 32, 40}; 			// Out from the station to the racetrack.
+	public static final int[] STATION_ENTRANCES = {4, 12, 20, 28, 36, 44};		// In from the racetrack to the station.
 }
