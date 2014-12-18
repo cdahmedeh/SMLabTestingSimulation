4a2f9720c71f62616197a1d80c97b1415f995b42
Ahmed El-Hajjar <me@cdahmedeh.net>
2014-12-17 19:10:22 -0500

    Comments update for new behaviors.

--- SMLabTestingSimulation_ABSModJ/src/org/smlabtesting/simabs/action/Arrival.java
+++ SMLabTestingSimulation_ABSModJ/src/org/smlabtesting/simabs/action/Arrival.java
@@ -9,7 +9,8 @@ import org.smlabtesting.simabs.model.SMLabModel;
 import absmodJ.ScheduledAction;
 
 /**
- * This action describes when a new sample arrives to the new samples queue.
+ * This action describes when a new sample arrives into one of the
+ * new samples queues.
  * 
  * Participants: Q.NewSamples
  * Uses: iC.Sample (implicit)
@@ -29,13 +30,14 @@ public class Arrival extends ScheduledAction {
 
 	@Override
 	protected void actionEvent() {
-        // Create a new sample with a randomly derived sequence and priority
+        // Create a new sample with a randomly derived sequence and priority.
         ICSample icSample = new ICSample();
         icSample.testsRemaining = model.rvp.uSequenceOfTests();
         icSample.rush = model.rvp.uSampleRush();
         icSample.startTime = model.getClock();
 		
-        // Move the new sample into the new samples queue.
+        // Move the new sample into the appropriate new samples queue based on
+        // it's priority.
         if( icSample.rush ) {
         	model.qNewSamples[RUSH].insertQue(icSample);
         } else {
