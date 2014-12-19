ac29be7d7c51358f51335f2903eb733359f54ac5
Ahmed El-Hajjar <me@cdahmedeh.net>
2014-12-19 16:54:11 -0500

    Fixed R calculation with lazy fix.

--- SMLabTestingSimulation_ABSModJ/src/org/smlabtesting/simabs/types/SuperConfidenceInterval.java
+++ SMLabTestingSimulation_ABSModJ/src/org/smlabtesting/simabs/types/SuperConfidenceInterval.java
@@ -7,9 +7,9 @@ import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.apache.commons.math3.stat.descriptive.StatisticalSummary;
 
 //TODO: Change to a newer joke.
+//TODO: cache zeta
 public class SuperConfidenceInterval {
 	private StatisticalSummary stats;
-	private double zeta = 0.0;
 	private double confidence = 0.0;
 
 	public SuperConfidenceInterval(double[] values, double confidence) {
@@ -35,19 +35,19 @@ public class SuperConfidenceInterval {
 				1 - (1 - confidence) / 2
 		);
 		
-		return zeta = tInv * stats.getStandardDeviation() / sqrt(stats.getN());
+		return tInv * stats.getStandardDeviation() / sqrt(stats.getN());
 	}
 	
 	public double getLowerCI() {
-		return stats.getMean() - zeta;
+		return stats.getMean() - getZeta();
 	}
 
 	public double getUpperCI() {
-		return stats.getMean() + zeta;
+		return stats.getMean() + getZeta();
 	}
 	
 	public double getR() {
-		return Math.abs(zeta/stats.getMean());
+		return Math.abs(getZeta()/stats.getMean());
 	}
 }
 
